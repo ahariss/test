3e564400638cc0c833dee6c9e356e721cdb8c0fb
ahariss <ahariss@borninteractive.com>
2022-10-23 13:06:02 +0300

    created AppModule for injection

---
+++ app/src/main/java/com/ahariss/test/mvvm/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.ahariss.test.mvvm.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
